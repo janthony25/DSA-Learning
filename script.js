edbd88870ac6cc3b2eea5c9834a70d1ab76cb625
Jun Anthony <junanthony.business@gmail.com>
2025-04-18 13:56:02 +1200

    add insert to linkedList

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -30,10 +37,56 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while(currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return array;
+  }
+
+  // I need to be able to add a value in a target index
+  insert(index, value) {
+    if(index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null
+    };
+
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    // check params
+
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(24)
-console.log(myLinkedList)
+myLinkedList.insert(2, 99);
+myLinkedList.insert(200, 88)
+console.log(myLinkedList.printList());
+// myLinkedList.insert(2, 99);
+// console.log(myLinkedList)
