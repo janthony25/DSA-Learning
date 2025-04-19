9fd73b5b166e3a0717590d5e69cf48b84b8a6188
Jun Anthony <junanthony.business@gmail.com>
2025-04-20 10:41:42 +1200

    Anagram challenge

--- codequiz.js
+++ codequiz.js
@@ -1,23 +1,55 @@
-// Two Sum
+// " Are Two Strings Anagrams?
 
-// Return the indices of two numbers that add up to the target.
+// ❓ Question:
 
-// Input: [2, 7, 11, 15], target = 9
-// Output: [0, 1]"
- 
-function twoSum(array, target) {
+// Check if two strings are anagrams (same letters, same frequency).
+
+// 3. Are Two Strings Anagrams?
+// ❓ Question:
+// Check if two strings are anagrams (same letters, same frequency)."
+
+function isAnagram(str1, str2) {
     const map = {};
 
-    for (let i = 0; i < array.length; i++) {
-        const complement = target - array[i];
+    if (str1.length !== str2.length) return false;
 
-        if(map[complement] !== undefined) {
-            return [map[complement], i];
+    for (let i = 0; i < str1.length; i++) {
+        const char = str1[i];
+
+        if (!map[char]) {
+            map[char] = 1;
         }
         else {
-            map[array[i]] = i;
+            map[char]++;
         }
     }
+
+    for (let i = 0; i < str2.length; i++) {
+        const char = str2[i];
+
+        if (!map[char]) {
+            return false;
+        }
+
+        map[char]--;
+    }
+
+    return true;
 }
 
-console.log(twoSum([2, 6, 11, 7, 15], 9))
+console.log(isAnagram('asd', 'dsa'))
+/*
+str1: l i s t e n --- str2: silent   -> true
+      0 1 2 3 4 5 
+                    checking....
+      a s d     ----  f s d
+      0 1 2     ----  0 1 2
+
+      map { a: 1, s: 1, d: 1}
+compare     f
+            listen      s i l e n t 
+                        . . . . . .
+     map { l: 0, i: 0, s: 0, t: 0, e: 0, n: 0 }
+compare    
+
+*/
