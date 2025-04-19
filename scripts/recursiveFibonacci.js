ba2d0242115afbadc4868815d54fb4fae76a3654
Jun Anthony <junanthony.business@gmail.com>
2025-04-20 01:24:59 +1200

    Bubble Sort lesson

--- scripts/recursiveFibonacci.js
+++ scripts/recursiveFibonacci.js
@@ -7,17 +7,25 @@
 
 
 function fibonacciIterative(n) {
-    // code here
-    let array = [0, 1];
+    // returns value of index
+    // const array = [0,1];
 
-    for (let i = 2; i < n + 1; i++) {
-        array.push(array[i -2 ] + array[i - 1])
+    // for (let i =2; i <= n; i++) {
+    //     array.push(array[i - 1] + array[i -2]);
+    // }
+
+    // return array[n];
+
+    // returns array
+    const array = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        array.push(array[i -1] + array[i -2]);
     }
-
-    return array[n]
+    return array;
 }
 
-// console.log(fibonacciIterative(8))
+console.log(fibonacciIterative(8))
 
 function fibonacciIterativeRecursive(n) {
     if (n < 2) {
@@ -26,4 +34,4 @@ function fibonacciIterativeRecursive(n) {
 
     return fibonacciIterativeRecursive(n -1) + fibonacciIterativeRecursive(n -2);
 }
-console.log(fibonacciIterativeRecursive(8))
+// console.log(fibonacciIterativeRecursive(8))
