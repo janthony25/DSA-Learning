ba8db48eea1c505eb565e21c91fa1f636c9afed3
Jun Anthony <junanthony.business@gmail.com>
2025-04-20 11:01:40 +1200

    longest substring challenge

--- codequiz.js
+++ codequiz.js
@@ -1,55 +1,37 @@
-// " Are Two Strings Anagrams?
+// "
+// USE SET()
+// 5. Longest Substring Without Repeating Characters
 
 // ❓ Question:
+// Return the length of the longest substring with no repeating characters.
 
-// Check if two strings are anagrams (same letters, same frequency).
 
-// 3. Are Two Strings Anagrams?
-// ❓ Question:
-// Check if two strings are anagrams (same letters, same frequency)."
+// Input: ""abcabcbb""
+// Output: 3"
 
-function isAnagram(str1, str2) {
-    const map = {};
+function longestSubstring(str) {
+    let longestStr = 0;
+    let set = new Set();
+    
+    let left = 0;
+    let right = 0;
 
-    if (str1.length !== str2.length) return false;
+    while(right < str.length) {
+        const char = str[right];
 
-    for (let i = 0; i < str1.length; i++) {
-        const char = str1[i];
-
-        if (!map[char]) {
-            map[char] = 1;
+        if(!set.has(char)) {
+            set.add(char);
+            longestStr = Math.max(longestStr, set.size);
+            right++;
         }
         else {
-            map[char]++;
+            const char = str[left];
+            set.delete(char);
+            left++;
         }
     }
 
-    for (let i = 0; i < str2.length; i++) {
-        const char = str2[i];
-
-        if (!map[char]) {
-            return false;
-        }
-
-        map[char]--;
-    }
-
-    return true;
+    return longestStr;
 }
 
-console.log(isAnagram('asd', 'dsa'))
-/*
-str1: l i s t e n --- str2: silent   -> true
-      0 1 2 3 4 5 
-                    checking....
-      a s d     ----  f s d
-      0 1 2     ----  0 1 2
-
-      map { a: 1, s: 1, d: 1}
-compare     f
-            listen      s i l e n t 
-                        . . . . . .
-     map { l: 0, i: 0, s: 0, t: 0, e: 0, n: 0 }
-compare    
-
-*/
+console.log(longestSubstring("abcabcbb"))
