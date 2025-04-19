07e15877de2c12b5f6eb7a2311c9a0ee54bf8362
Jun Anthony <junanthony.business@gmail.com>
2025-04-19 16:01:59 +1200

    twosum challenge

--- codequiz.js
+++ codequiz.js
@@ -1,26 +1,21 @@
-// " 1.
-//     First recurring number
- 
-//     Given an array = [2, 5, 1, 2, 3, 5, 1, 2, 4];
-//     It should return 2
+// Two Sum
 
-//     Given an array = [2, 1, 1, 2, 3, 5, 1, 2, 4];
-//     It should return 1
+// Return the indices of two numbers that add up to the target.
 
-//     Given an array = [2, 3, 4, 5]
-//     It should return undefined"""
-
-function firstRecurringNumber(array) {
+// Input: [2, 7, 11, 15], target = 9
+// Output: [0, 1]"
+function twoSum(nums, target) {
     const map = {};
 
-    for(let i = 0; i < array.length; i++) {
-        if(map[array[i]] !== undefined) {
-            return array[i];
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        console.log('hmm:', complement)
+        if (map[complement] !== undefined) {
+            console.log("complement", map[complement])
+            return [map[complement], i];
         }
-        else {
-            map[array[i]] = i
-        }
+        map[nums[i]] = i;
     }
 }
 
-console.log(firstRecurringNumber([2, 1, 1, 2, 3, 5, 1, 2, 4]))
+console.log(twoSum([2, 4, 7, 11, 15], 9))
