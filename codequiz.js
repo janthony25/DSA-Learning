f1a6c28952dd093dd164310195aeaa0fb17ac392
Jun Anthony <junanthony.business@gmail.com>
2025-04-19 14:12:32 +1200

    reverse string challenge

--- codequiz.js
+++ codequiz.js
@@ -1,25 +1,11 @@
-// Remove Duplicates from Sorted Array
-// Input: [1,1,2,3,3]
-// Output: [1,2,3]
+function reverse(str) {
+    const reversed = [];
+    let charLength = str.length - 1;
 
-function removeDuplicates(nums) {
-    if (nums.length === 0) return 0;
-
-    let slow = 0;
-
-    for (let fast = 1; fast < nums.length; fast++) {
-        if (nums[fast] !== nums[slow]) {
-            slow++;
-            nums[slow] = nums[fast];
-        }
+    for (let i = charLength; i >= 0; i--) {
+        reversed.push(str[i]);
     }
 
-    return slow + 1; // Count of unique elements
+    return reversed.join('')
 }
-
-// Example usage
-const arr = [1, 1, 2, 3, 3];
-const length = removeDuplicates(arr);
-
-console.log("Unique count:", length);              // Output: 3
-console.log("Result:", arr.slice(0, length));      // Output: [1, 2, 3]
+console.log(reverse("Hello!"))
