fc8dc7cfbe2e378e10d3ef906d110e068caccf79
Jun Anthony <junanthony.business@gmail.com>
2025-05-30 00:07:03 +1200

    factorial number

--- try.js
+++ try.js
@@ -1,21 +1,11 @@
-function binarySearch(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+function factorial(n) {
+    let result = 1;
 
-    while (left <= right) {
-        let mid = left + Math.floor((right - left) / 2);
-
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] > target) {
-            right = mid - 1;
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        }
+    for (let i = 2; i <= n; i++) {
+        result *= i;
     }
 
-    return -1;
-
+    return result;
 }
 
-console.log(binarySearch([-1, 0, 3, 5, 9, 12], 9));
+console.log(factorial(5));
