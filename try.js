b79759154212fb86638ff9f9457375baefe11428
Jun Anthony <junanthony.business@gmail.com>
2025-05-30 22:57:45 +1200

    is palindrome

--- try.js
+++ try.js
@@ -1,20 +1,18 @@
-function twoSum(nums, target) {
-    let left = nums[0];
-    let right = nums.length - 1;
+function isPalindrome(str){
+    str = str.toLowerCase();
 
-    while (left < right) {
-        let sum = nums[left] + nums[right];
+    let left = 0;
+    let right = str.length - 1;
 
-        if (sum > target) {
-            right--;
-        } else if (sum < target) {
-            left++;
-        } else {
-            return [left, right];
+    while(left < right) {
+        if (str[left] !== str[right]) {
+            return false;
         }
+        left++;
+        right--;
     }
+    return true;
 
-    return null;
 }
 
-console.log(twoSum([1, 2, 4, 4], 8));
+console.log(isPalindrome("car"));
