c0ff15af6eae1d26c375d687354dee81a4633102
Jun Anthony <junanthony.business@gmail.com>
2025-06-01 11:40:15 +1200

    merge 2 sorted array

--- try.js
+++ try.js
@@ -1,11 +1,28 @@
-const findFactorial = (n) => {
-    let result = 1;
+const mergeTwoArray = (arr1, arr2) => {
+  const mergedArr = [];
+  let i = 0, j =0;
 
-    for(let i = 2; i <= n; i++) {
-        result *= i;
+  while (i <arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+        mergedArr.push(arr1[i]);
+        i++;
+    } else {
+        mergedArr.push(arr2[j]);
+        j++;
     }
+  }
 
-    return result;
+  while (i < arr1.length) {
+    mergedArr.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    mergedArr.push(arr2[j]);
+    j++;
+  }
+
+  return mergedArr;
 }
 
-console.log(findFactorial(5));
+console.log(mergeTwoArray([4,9], [2, 5, 7, 10]));
