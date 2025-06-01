9445058c90b69bfa4b03e6da4c9d55165410ca7d
Jun Anthony <junanthony.business@gmail.com>
2025-06-01 20:40:19 +1200

    move zeroes in front of the array

--- try.js
+++ try.js
@@ -1,28 +1,17 @@
-const mergeTwoArray = (arr1, arr2) => {
-  const mergedArr = [];
-  let i = 0, j =0;
+const moveZeroes = (arr) => {
+  let left = 0;
 
-  while (i <arr1.length && j < arr2.length) {
-    if (arr1[i] <= arr2[j]) {
-        mergedArr.push(arr1[i]);
-        i++;
-    } else {
-        mergedArr.push(arr2[j]);
-        j++;
+  for (let right = 0; right < arr.length; right++) {
+    if (arr[right] !== 0) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
     }
   }
 
-  while (i < arr1.length) {
-    mergedArr.push(arr1[i]);
-    i++;
-  }
-
-  while (j < arr2.length) {
-    mergedArr.push(arr2[j]);
-    j++;
-  }
-
-  return mergedArr;
+  return arr;
 }
 
-console.log(mergeTwoArray([4,9], [2, 5, 7, 10]));
+
+console.log(moveZeroes([0, 1, 0, 3, 12, 0]));
