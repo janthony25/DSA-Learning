42d46ce8ef84e731386dbeaeb7f444f06ef51773
Jun Anthony <junanthony.business@gmail.com>
2025-06-02 23:13:07 +1200

    DSA fibonacci

--- try.js
+++ try.js
@@ -1,17 +1,28 @@
-const moveZeroes = (arr) => {
-  let left = 0;
+const fibonacci = (num) => {
+  let curr = 1;
+  let prev = 0;
 
-  for (let right = 0; right < arr.length; right++) {
-    if (arr[right] !== 0) {
-        let temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-        left++;
-    }
+  for (let i = 2; i <= num; i++) {
+    let temp = curr;
+    curr = prev + curr;
+    prev = temp;
   }
-
-  return arr;
+  return curr;
 }
 
+console.log(fibonacci(6))
 
-console.log(moveZeroes([0, 1, 0, 3, 12, 0]));
+
+
+/*
+fibonacci = [0 , 1, 1, 2, 3, 5, 8]
+             0   1  2  3  4  5  6
+
+input : 3
+output: 2
+
+time complexity: O(n)
+space complexity: O(1)
+
+
+*/
