b8a8f9febc207c6acb632e1d659a2aa8c193a8c2
Jun Anthony <junanthony.business@gmail.com>
2025-06-02 23:59:34 +1200

    DSA ispalindrome

--- try.js
+++ try.js
@@ -1,28 +1,16 @@
-const fibonacci = (num) => {
-  let curr = 1;
-  let prev = 0;
+function isPalindrome(num) {
+  if (num < 0) return false;
 
-  for (let i = 2; i <= num; i++) {
-    let temp = curr;
-    curr = prev + curr;
-    prev = temp;
+  const original = num;
+  let reversed = 0;
+
+  while (num > 0) {
+    let digit = num % 10;
+    reversed = reversed * 10 + digit;
+    num = Math.floor(num / 10);
   }
-  return curr;
+
+  return original === reversed;
 }
 
-console.log(fibonacci(6))
-
-
-
-/*
-fibonacci = [0 , 1, 1, 2, 3, 5, 8]
-             0   1  2  3  4  5  6
-
-input : 3
-output: 2
-
-time complexity: O(n)
-space complexity: O(1)
-
-
-*/
+console.log(isPalindrome(121));
